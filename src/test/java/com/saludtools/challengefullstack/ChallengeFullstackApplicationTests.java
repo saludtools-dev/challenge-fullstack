bf8226c213e463aa1c26bc485acf3c30f527c8e7
Luisa Ramirez <luisa.ramirez@gopass.com.co>
2022-11-01 22:20:54 -0500

    C1-creacion de proyecto

---
+++ src/test/java/com/saludtools/challengefullstack/ChallengeFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saludtools.challengefullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
